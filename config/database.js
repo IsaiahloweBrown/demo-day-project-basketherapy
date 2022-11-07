55abcfe591eee15552f564456a090da40e044ef4
Isaiah Lowe-Brown <isaiahlowebrown@gmail.com>
2022-11-07 00:42:51 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://sixers:ulAmPUYkMt7MxZOM@cluster0.srifgsf.mongodb.net/basketherapy?retryWrites=true&w=majority', 
+    'dbName': 'basketherapy',
+    
+};
+
+
+
+// 'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+//     'dbName': 'demo'
